9af0190815d18d3b6d27487128c9eb7cce419c2e
Yusufxon <246614796+suniyintelektuz-byte@users.noreply.github.com>
2026-05-16 12:27:50 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,18 @@
 
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# Railway-dagi ma'lumotlar bazasi URL manzilini oladi, 
+# bo'lmasa vaqtincha sqlite ishlatadi
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
+
+# Aynan shu 'engine' nomi xatolikni yo'qotadi
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
